f50c15b0ef2c7aada2d29284a91aef2a6780c6ac
[jg684] <[jg684@kent.ac.uk]>
2024-08-08 20:33:59 +0100

    Revert "Completed stringStripper method"
    This reverts commit 5e5b988bc491e9382a767f92c9d2c2bb49a69f6c.

--- src/main/java/PalindromeTester.java
+++ src/main/java/PalindromeTester.java
@@ -1,23 +1,17 @@
 public class PalindromeTester {
 
-    /**
-     * Checks to see if the given string is a palindrome (Ignores spaces, caps and punctuation)
-     * @param palindrome the string to test
-     * @return whether the string is a palindrome
-     */
     public boolean palindromeTester(String palindrome) {
         return false;
     }
 
     /**
      * Removes spaces, capitalisation and punctuation from given string
-     * @param string the string to strip
+     * @param string string to strip
      * @return stripped string
      */
-    String stringStripper(String string) {
-        var lowercaseString = string.toLowerCase();
+    public String stringStripper(String string) {
+        var withoutSpaces = string.replaceAll("\s", "");
 
-        // Uses a combined regex to match punctuation: \\p{Punct} and spaces: \s
-        return lowercaseString.replaceAll("[\\p{Punct}\s]", "");
+        return withoutSpaces;
     }
 }
