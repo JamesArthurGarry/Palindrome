85eb57eacb212dca09aac5136723e1921f8ca7ee
[jg684] <[jg684@kent.ac.uk]>
2024-08-08 21:11:39 +0100

    Completed checkPalindrome method - Renamed method - Added code to compare the first and second half of the stripped given string - Added tests

--- src/main/java/PalindromeTester.java
+++ src/main/java/PalindromeTester.java
@@ -2,11 +2,28 @@ public class PalindromeTester {
 
     /**
      * Checks to see if the given string is a palindrome (Ignores spaces, caps and punctuation)
-     * @param palindrome the string to test
+     * @param possiblePalindrome the string to test
      * @return whether the string is a palindrome
      */
-    public boolean palindromeTester(String palindrome) {
-        return false;
+    public boolean checkPalindrome(String possiblePalindrome) {
+
+        // Remove ignored characters
+        var strippedString = stringStripper(possiblePalindrome);
+
+        // If the length of the stripped given string is 0 or 1 then it is automatically a palindrome
+        var stringLength = strippedString.length();
+        if (stringLength < 2) {
+            return true;
+        }
+
+        // Split the string in half and check if the first half is the same as the second reversed
+        int mid = stringLength / 2;
+        var firstHalf = strippedString.substring(0, mid);
+        // If odd number of characters then don't include the middle as it's not necessary to be checked
+        // i.e racecar -> rac e car, only 'rac' and 'car' have to be compared
+        var secondHalf = strippedString.substring(stringLength % 2 == 1 ? mid + 1 : mid);
+
+        return firstHalf.equals(new StringBuilder(secondHalf).reverse().toString());
     }
 
     /**
